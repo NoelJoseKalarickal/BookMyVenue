a47944e667878ae72ab88a0baf021d83ff7feeaf
Noel Jose Kalarickal <noel.jk2312@gmail.com>
2026-07-22 13:05:49 +0530

    Set up initial project structure with different apps

--- bookmyvenue/settings.py
+++ bookmyvenue/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accounts',
+    'venues',
+    'bookings',
+    'payments',
+    'reviews',
+    'maintenance',
+    'audit',
 ]
 
 MIDDLEWARE = [
